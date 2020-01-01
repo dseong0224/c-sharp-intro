bbbacd8cab163c3e2a58e2451719ca7ce6fad7c4
Daniel Seong <dseong0224@gmail.com>
2019-12-31 22:44:40 -0800

    declared first variable using visual studio mac

---
+++ HelloWorld/HelloWorld/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			byte number = 2;
+			int count = 10;
+			float totalPrice = 20.95;
+
+			Console.WriteLine(number);
+
+        }
+    }
+}
